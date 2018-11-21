9fe16572ae158e4d5191cc2144a849a0e6c366b1
ritchey0713 <david@osa.io>
2018-11-21 10:49:45 -0500

    add action amiler

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,22 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = {host: '127.0.0.1' }
+    #add the aws url here!
+
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication  => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'my url here',
+    :enable_starttls_auto => true
+  }
+
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
